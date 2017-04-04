5abd3573e4f10f89bd44821c475836cb5203beb7
unbalanced <unbalanced@unbalanceds-MacBook.local>
2017-04-04 11:32:36 -0300

    Add basic working proof of concept

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::net::UdpSocket;
+use std::io;
+use std::io::Write;
+use std::thread;
+
+fn main () {
+    let child = thread::spawn(move || {
+        let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
+        socket.set_broadcast(true).expect("set_broadcast call failed");
+
+        print!("> ");
+        io::stdout().flush().unwrap_or_default();
+
+        loop {
+
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    input.pop();
+
+                    process(&socket, input);
+                    print!("> ");
+                    io::stdout().flush().unwrap_or_default();
+                }
+                Err(error) => println!("error: {}", error),
+            }
+        }
+    });
+
+    let child2 = thread::spawn(move || {
+        loop {
+            let socket = UdpSocket::bind("0.0.0.0:4041").expect("couldn't bind to address");
+            let mut buf = [0; 10];
+            let (_, _) = socket.recv_from(&mut buf)
+                                                .expect("Didn't receive data");
+
+            println!("{:?}", buf);
+        }
+    }); 
+    
+    child.join().unwrap();
+    child2.join().unwrap();
+}
+
+fn process(socket: &UdpSocket, input: String) {
+    match input.as_ref() {
+        "" => {
+            ()
+        },
+        "/probe" => {
+            let message = b"P";
+            socket.send_to(message, "255.255.255.255:4041").expect("couldn't send data");
+            ()
+        },
+        _ => {
+            let message = format!("{}{}", "M", input);
+            socket.send_to(&message.into_bytes(), "255.255.255.255:4041").expect("couldn't send data");           
+            ()
+        }
+    }
+}
